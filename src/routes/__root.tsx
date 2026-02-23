f1529f04dd5684f58a0340e87ab04c16058d2d67
Marius Eikenes <eikenes.marius@gmail.com>
2026-02-23 19:28:11 +0100

    tracking

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,25 +1,27 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { Databuddy } from '@databuddy/sdk/react';
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <Databuddy clientId="81e39087-196e-4f11-966b-f7f07b677129" />
       <Header />
       <Outlet />
       <TanStackDevtools
         config={{
-          position: 'bottom-right',
+          position: "bottom-right",
         }}
         plugins={[
           {
-            name: 'Tanstack Router',
+            name: "Tanstack Router",
             render: <TanStackRouterDevtoolsPanel />,
           },
         ]}
       />
     </>
   ),
-})
+});
