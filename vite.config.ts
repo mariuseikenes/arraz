62d01eb6efe98c75115fb8083257d8b5b677fbd1
Marius Eikenes <hello@mariuseikenes.com>
2026-03-02 15:32:26 +0100

    Sitemap + rulebook

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr"
 import tailwindcss from '@tailwindcss/vite'
+import Sitemap from "vite-plugin-sitemap"
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { fileURLToPath, URL } from 'node:url'
@@ -18,6 +19,22 @@ export default defineConfig({
     viteReact(),
     tailwindcss(),
     svgr(),
+    Sitemap({ 
+      hostname: 'https://arraz.app',
+
+      dynamicRoutes: [
+        '/',
+        '/rules', // Or whatever your rulebook URL is
+        '/setup',
+        '/about',
+        '/contact',
+        '/privacy-policy',
+        '/tos',
+        '/x01',
+        '/killer',
+        '/rules/x01'
+      ],
+    })
   ],
   resolve: {
     alias: {
